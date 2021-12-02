4a70a65c087cda975074934164d6c16c1d40d128
donpark <pdh6381@naver.com>
2021-12-02 20:52:27 +0900

    02-ray

---
+++ raytracing/01/source/01_rgb_vector.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "keyhook.h"
+#include "vec3.h"
+#include "color.h"
+
+int	main(void)
+{
+	t_data	data;
+
+	int		win_width = 256 * 2;
+	int		win_height = 256 * 2;
+	int		img_width = win_width;
+	int		img_height = win_height;
+	data.mlx = mlx_init();
+	data.win = mlx_new_window(data.mlx, win_width, win_height, "Tutorial");
+	data.img = mlx_new_image(data.mlx, img_width, img_height);
+	data.addr = mlx_get_data_addr(data.img, &data.bits_per_pixel, &data.line_length, &data.endian);
+	mlx_hook(data.win, KEY_PRESS, 1L<<0, key_hook, &data);
+	//////////////////////////////////////////////////////////////////
+	int i, j;
+	for (j = img_height - 1; j >= 0 ; j--) {
+		fprintf(stderr, "Scanlines remaining: %d\n", j);	fflush(stderr);
+		for (i = 0; i < img_width; i++) {
+			t_color pixel_color = vec3_((double)i / (img_width - 1), (double)j / (img_height - 1), 0.3);
+			input_color(&data, pixel_color);
+			my_mlx_pixel_put(&data, i, j, data.color);
+		}
+	}
+	fprintf(stderr, "\nDone.\n");	fflush(stderr);
+	////////////////////////////////////////////////////////////////
+	mlx_put_image_to_window(data.mlx, data.win, data.img, 0, 0);
+	mlx_loop(data.mlx);
+	return (0);
+}
---
+++ raytracing/02/source/keyhook.c
@@ -0,0 +1,21 @@
+#include "keyhook.h"
+#include <stdlib.h>
+
+void close_screen(t_data *data)	// 이벤트 발생시 call될 함수
+{
+	mlx_destroy_window(data->mlx, data->win);	// 창을 닫는다.
+	/** [ ADD in Linux or Ubuntu ]
+	 * mlx_destroy_image(data->mlx, data->img);
+	 * mlx_destroy_display(data->mlx);
+	 * free(data->mlx);
+	 */
+	system("leaks tutorial");	// memory leaks check
+	exit(EXIT_SUCCESS);
+}
+
+int	key_hook(int keycode, t_data *data)
+{
+	if (keycode == ESC)
+		close_screen(data);
+	return (0);
+}
