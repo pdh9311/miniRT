7641ed192941eb7414c4fe0cbd04da6dbe948048
Joonsoo Park <joonpark@c10r4s9.42seoul.kr>
2021-12-13 20:10:07 +0900

    ambient + test_file

--- source/ray_tracing/ambient.c
+++ source/ray_tracing/ambient.c
@@ -2,10 +2,8 @@
 
 void	set_ambient(t_color *pixel_color, t_scene *scene, t_hit_record *rec)
 {
+	multiply_(&scene->ambient.color, scene->ambient.bright_ratio);
 	add_(pixel_color, scene->ambient.color);
-	multiply_(pixel_color, scene->ambient.bright_ratio);
-	//multiply_(&scene->ambient.color, scene->ambient.bright_ratio);
-	//add_(pixel_color, scene->ambient.color);
 	*pixel_color = multiply__(*pixel_color, rec->albedo);
 	vec_min(pixel_color, (t_color){1.0, 1.0, 1.0});
 }
